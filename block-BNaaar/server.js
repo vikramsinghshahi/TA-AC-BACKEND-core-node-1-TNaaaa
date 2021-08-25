1583a371064610f15cc740f78b1b8478bef94a14
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-08-25 21:48:58 +0530

    update

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -3,7 +3,18 @@ var http = require('http');
 var server = http.createServer(handleServer);
 
 function handleServer(req, res) {
-  res.end('This is the end');
+  if (req.method === 'GET' && req.url === '/') {
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Welcome to home page');
+  } else if (req.method === 'GET' && req.url === '/about') {
+    res.setHeader('Content-type', 'text/plain');
+    res.end('<h2>This is about page</h2>');
+  } else if (req.method === 'POST' && req.url === '/about') {
+    res.setHeader('Content-type', 'application/json');
+    res.end('{message: this is a post request}');
+  }
 }
 
-server.listen(5000, () => console.log('server is running'));
+server.listen(5000, () => {
+  console.log('server is running');
+});
