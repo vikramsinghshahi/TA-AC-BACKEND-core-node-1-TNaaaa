e97cee989bbde2dfd7f5e58010f62f9b9aa77cd8
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-08-27 12:33:02 +0530

    update

--- block-BNaaau/server.js
+++ block-BNaaau/server.js
@@ -8,36 +8,178 @@ var server = http.createServer(handleServer);
 //   - do a request using browser on `localhost:5000`
 //   - check out console for request and response object
 
-function handleServer(req, res) {
-  console.log(req, res);
-  res.end('welcome');
-}
+// function handleServer(req, res) {
+//   console.log(req, res);
+//   res.end('welcome');
+// }
 
-server.listen(5000, () => {
-  console.log('server is running');
-});
+// server.listen(5000, () => {
+//   console.log('server is running');
+// });
 
 // Q. create a node server
 //   - add listener on port 5100
 //   - respond with 'My first server in NodeJS' using response object
 
-function handleServer(req, res) {
-  res.end('My first server in NodeJS');
-}
+// function handleServer(req, res) {
+//   res.end('My first server in NodeJS');
+// }
 
-server.listen(5100, () => {
-  console.log('server is ruunnig');
-});
+// server.listen(5100, () => {
+//   console.log('server is ruunnig');
+// });
 
 // Q. write code to create a node server
 //   - add listener on port 5555
 //   - console request headers
 //   - respond with content of user-agent from request headers.
 
+// function handleServer(req, res) {
+//   console.log(req.headers);
+//   res.end(req.headers['user-agent']);
+// }
+
+// server.listen(5555, () => {
+//   console.log('server is ruunnig');
+// });
+
+// writeCode
+
+// Q. write code to create a node server
+//   - add listener on port 5566
+//   - console request url and request method
+//   - return a text response with requested url and method
+
+// function handleServer(req, res) {
+//   console.log(req.method, req.url);
+//   res.end(req.method + req.url);
+// }
+
+// server.listen(5566, () => {
+//   console.log('server is ruunnig');
+// });
+
+// Q. write code to create a node server
+//   - add listener on port 7000
+//   - also add a callback function to listener with a console `server listening on port 7000`
+//   - return entire request headers in response.
+
+// function handleServer(req, res) {
+//   res.end(JSON.stringify(req.headers));
+// }
+
+// server.listen(7000, () => {
+//   console.log('server is listening on port 7000');
+// });
+
+// Q. create a server
+//   - add a listener on port 3333
+//   - set status code 202 in response using `res.statusCode`.
+
+// function handleServer(req, res) {
+//   res.statusCode = 200;
+//   res.end(JSON.stringify(req.headers));
+// }
+
+// server.listen(3333, () => {
+//   console.log('server is listening on port 7000');
+// });
+
+// Q. create a server
+//   - add a listener on port 8000
+//   - set appropriate header for html response using `res.setHeader`
+//   - return an HTML response(`<h3>Welcome to altcampus</h3>`)
+
+// function handleServer(req, res) {
+//   res.setHeader('content', 'text/html');
+//   res.end('<h3>Welcome to altcampus</h3>');
+// }
+
+// server.listen(8000, () => {
+//   console.log('server is listening on port 8000');
+// });
+
+// Q. Repeat above question using `res.writeHead` to set status code and Content-Type at the same time.
+
+// Q. create a basic node server
+//   - add a listener at port 8888
+//   - add appropriate header for json response
+//   - send json response({success: true, message: 'Welcome to Nodejs'})
+
+// function handleServer(req, res) {
+//   res.writeHead(201, 'content-type', 'application/json');
+//   res.end(
+//     JSON.stringify({
+//       success: true,
+//       message: 'Welcome to Nodejs',
+//     })
+//   );
+// }
+
+// server.listen(8888, () => {
+//   console.log('server is listening on port 8888');
+// });
+
+// Q. create a server
+//   - add listener on port 5050
+//   - use postman to do a POST request on index route
+//   - console to check request method
+//   - send HTML response i.e. `<h2>posted for first time</h2>`
+
+// function handleServer(req, res) {
+//   console.log(req.method);
+//   res.writeHead(201, 'content-type', 'text/html');
+//   res.end(`<h2>posted for first time</h2>`);
+// }
+
+// server.listen(5050, () => {
+//   console.log('server is listening on port 8888');
+// });
+
+// Q. create a server and handle 2 different requests
+//   - add a listener on port 2345
+//   - handle GET request on '/' route where return your name in plain text in response
+//   - handle GET request on '/about' route and return your name in h2 as HTML page.
+//   - add a error handler at last to handle request made on other than above routes with a status code of 404.
+
+// function handleServer(req, res) {
+//   if (req.method === 'GET' && req.url === '/') {
+//     res.setHeader('content-type', 'text/plain');
+//     res.end(`Vikram`);
+//   } else if (req.method === 'GET' && req.url === '/about') {
+//     res.setHeader('content-type', 'text/html');
+//     res.end(`<h2>Vikram</h2>`);
+//   } else {
+//     res.statusCode = 404;
+//     res.end(`Page Not Found`);
+//   }
+// }
+
+// server.listen(2345, () => {
+//   console.log('server is listening on port 2345');
+// });
+
+// Q. Handle 2 requests on same route with different method
+//     1. GET on '/users' route where return a simple HTML form with name and email field
+//     2. POST on '/users' route with a message 'Posted for the second time'.
+
+// Q. create a server and handle query params from the request on following url i.e. `/users?email=nodeserver@gmail.com` from browser
+
+//   - parse the  request url using parse method from url module
+//   - console pathname from parsed url in above step
+//   - grab url using `req.url`
+//   - differentiate between `req.url` and `parsedUrl.pathname`
+//   - grab the email from query params
+//   - return json response with email from query params
+
+var url = require('url');
 function handleServer(req, res) {
-  res.end('My first server in NodeJS');
+  var parsedUrl = url.parse(req.url, true);
+  console.log(parsedUrl.pathname, req.url);
+  res.setHeader('content-Type', 'application/json');
+  res.end(JSON.stringify(parsedUrl.query));
 }
 
-server.listen(5555, () => {
-  console.log('server is ruunnig');
+server.listen(2345, () => {
+  console.log('server is listening on port 2345');
 });
